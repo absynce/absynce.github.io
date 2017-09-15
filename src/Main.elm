ef872e4254320085a59a69178e1aec0eb824d29c
Jared M. Smith <absynce@gmail.com>
2017-09-14 20:46:43 -0500

    Add steps 1 and 2 to the blog app blog post

--- src/Main.elm
+++ src/Main.elm
@@ -76,15 +76,58 @@ pageToContent page =
 
 absynce developer blog written in Elm coming soon.
 
+I've been writing software for over 10 years. The beauty, simplicity and usefulness of [Elm](http://elm-lang.org/) is what brought me out of my clamshell and prompted me to write this.
+
 # Learning Elm by writing a SPA blog with Github pages
 
-I've been writing software for over 10 years. The beauty, simplicity, and usefulness of [Elm](http://elm-lang.org/) is what brought me out of my clamshell and prompted me to write this.
 
 Share your journey into Elm while learning it by creating an Elm SPA blog and hosting it for free on GitHub Pages.
 
-## Step 0.
+## Step 0. - Read _An Introduction to Elm_
 
 If you haven't walked through [An Introduction to Elm](https://guide.elm-lang.org/) do that now. It's easy and fun to read. By the end you'll have your environment set up and be ready to start writing your blog in Elm.
+
+## Step 1. - Create a GitHub page
+
+[Create a GitHub page](https://pages.github.com/) with your GitHub username: username.github.io. Clone your repo locally.
+
+## Step 2. - Add index.html
+
+Fire up your favorite text editor. Add the following to index.html in the root of your project:
+
+```
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>your blog title</title>
+    <script src="elm.js"></script>
+  </head>
+  <body id="page-body">
+    <script>
+      var app = Elm.Main.fullscreen(null);
+    </script>
+  </body>
+</html>
+```
+
+This does two important things for Elm:
+
+1. References your soon-to-be compiled Elm app JavaScript output
+```
+    <script src="elm.js"></script>
+```
+
+2. Tells Elm to run your app full screen.
+```
+    var app = Elm.Main.fullscreen(null);
+```
+
+<aside class="notice">
+    There's an intentional error in the JavaScript you'll fix once you've set up the Elm application.
+</aside>
+
+Now that you have a basic idea of the syntax and architecture of Elm apps
 
 ## Test Code Block
 
